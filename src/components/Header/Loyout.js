5108ccbf92a0f7ca14e7c3ece241b10dc3ff64d0
Andrey Uvarov <andrey-1@ukr.net>
2023-01-20 19:34:54 +0200

    commit6

--- src/components/Header/Loyout.js
+++ src/components/Header/Loyout.js
@@ -1,15 +1,6 @@
-// import { Outlet } from 'react-router-dom';
-// import { Toaster } from 'react-hot-toast';
-// import AppBar from './AppBar';
-// import { GlobalStyle } from './GlobalStyle';
-// import { Box } from './Box';
 import { Suspense } from 'react';
-
 import { Outlet } from 'react-router-dom';
 
-import 'react-toastify/dist/ReactToastify.css';
-import { ThreeDots } from 'react-loader-spinner';
-import { ToastContainer, toast } from 'react-toastify';
 import AppBar from './AppBar';
 
 export const Layout = () => {
@@ -19,8 +10,6 @@ export const Layout = () => {
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
-
-      {/* <Toaster position="top-right" reverseOrder={false} />  */}
     </>
   );
 };
